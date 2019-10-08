3e958758a9be76ae7fabb79b4566f41eb83e49c8
John Bachman <bachmanjohn@gmail.com>
2019-10-08 16:41:47 -0400

    Update

--- models/painmachine/build_pain_machine_prior.py
+++ models/painmachine/build_pain_machine_prior.py
@@ -1,16 +1,38 @@
-import os
-import indra
-import pandas
+#import os
+#import indra
+#import pandas
+from emmaa.priors import SearchTerm
 from emmaa.priors.gene_list_prior import GeneListPrior
-
 
 def get_pain_proteins():
     with open('pain_proteins.csv', 'rt') as f:
-        gene_names = [line.strip() for line in f.readlines()]
+        gene_names = []
+        for line in f.readlines():
+            s = line.strip()
+            if not s or s.startswith('#'):
+                continue
+            gene_names.append(s)
     return gene_names
+
+def get_pain_chemical_terms():
+    with open('pain_chemicals.csv', 'rt') as f:
+        chem_terms = []
+        for line in f.readlines():
+            s = line.strip()
+            if not s or s.startswith('#'):
+                continue
+            name, chebi_id = [t.strip() for t in s.split(',')]
+            term = SearchTerm(type='drug', name=name,
+                              search_term=f'"{name}"',
+                              db_refs={'CHEBI': chebi_id})
+            chem_terms.append(term)
+    return chem_terms
 
 
 if __name__ == '__main__':
     pain_proteins = get_pain_proteins()
     gp = GeneListPrior(pain_proteins, 'painmachine', 'Pain Machine')
+    gp.make_search_terms()
+    chem_terms = get_pain_chemical_terms()
+    gp.search_terms.extend(chem_terms)
     gp.make_model()
